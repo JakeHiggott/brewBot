b9bce5dc69604d698b881489dc91ee6b2d7da27c
Jake Higgott <jakehiggott1@gmail.com>
2021-04-30 16:12:26 -0400

    Added embedded labels to make bot look cleaner

--- brewBot.py
+++ brewBot.py
@@ -12,38 +12,89 @@ client = commands.Bot(command_prefix = '!')
 async def on_ready():
     print('Bot is Online.')
 
+@client.command()
+#this command will tell us the ping of the bot called with ".ping"
+async def ping(ctx):
+    title = f'pong! (Ping ms: {round(client.latency * 1000)}ms )'
+    message = 'Im hosted off a rasberry pi in the middle of nowhere. Sorry if Im slow :( '
+    embed = discord.Embed(
+    title = title,
+    description = message,
+    color = discord.Color.orange()
+    )
+    await ctx.send(embed=embed)
+    
+
 
 @client.command()
 async def brew(ctx,*,userInput):
+        title = ''
+        message = ''
         i=0 
         r = requests.get(f'https://api.openbrewerydb.org/breweries?by_postal={userInput}')
         if(len(r.json()) > 0):
-            await ctx.send(f"***brewBot Found {len(r.json())} results for postal code: {userInput}***")
+            title = f"***brewBot Found {len(r.json())} results for postal code: {userInput}***"
             while (i < len(r.json())):
-                await ctx.send(f"**Brewery:** {r.json()[i]['name']}\nStreet Adress: {r.json()[i]['street']}\n")
+                message = message + f"**Brewery:** {r.json()[i]['name']}\nStreet Adress: {r.json()[i]['street']}\n"
                 i = i+1
         else:
             r = requests.get(f'https://api.openbrewerydb.org/breweries?by_state={userInput}')
             if(len(r.json()) > 0):
-                await ctx.send(f"***brewBot Found {len(r.json())} results for the State: {userInput}***")
+                title = f"***brewBot Found {len(r.json())} results for the State: {userInput}***"
                 while (i < len(r.json())):
-                    await ctx.send(f"**Brewery:** {r.json()[i]['name']}\nStreet Adress: {r.json()[i]['street']}\n")
+                    message = message + f"**Brewery:** {r.json()[i]['name']}\nStreet Adress: {r.json()[i]['street']}\n"
                     i = i+1
             else:
                 r = requests.get(f'https://api.openbrewerydb.org/breweries?by_city={userInput}')
                 if(len(r.json()) > 0):
-                    await ctx.send(f"***brewBot Found {len(r.json())} results for the City: {userInput}***")
+                    title = f"***brewBot Found {len(r.json())} results for the City: {userInput}***"
                     while (i < len(r.json())):
-                        await ctx.send(f"**Brewery:** {r.json()[i]['name']}\nStreet Adress: {r.json()[i]['street']}\n")
+                        message = message + f"**Brewery:** {r.json()[i]['name']}\nStreet Adress: {r.json()[i]['street']}\n"
                         i = i+1
         if(i == 0):
-            await ctx.send(f"Sorry! I could not find anything. :(\nYou can give me a state, city, or zipcode and I will look for you. \nJust make sure it's spelled correctly!")
-        else:
-            await ctx.send(f"***Search Complete!***")
+            title = f"Sorry! I could not find anything. :(\nYou can give me a state, city, or zipcode and I will look for you. \nJust make sure it's spelled correctly!"
+       
+            
+
+        embed = discord.Embed(
+        title = title,
+        description = message,
+        color = discord.Color.orange()
+        )
+        await ctx.send(embed=embed)
+
+
+
+client.run('Token here')    
+
+
+"""
+@client.command()
+async def displayembed(ctx):
+    description1 = "DESCRIPTION 1"
+    description2 = "DESCRIPTION 2"
+    desCat = description1 + description2
+    embed = discord.Embed(
+        title = 'Title',
+        description = desCat,
+        color = discord.Color.blue()
+    )
+   
+    embed.set_footer(text='This is a footer.')
+    #embed.set_image()
+    #embed.set_thumbnail()
+    #embed.set_author()
+    #embed.add_field(name = "ID", value = member.id, inline= True)
+    await ctx.send(embed=embed)
+
+
+"""
+
+
 
         
 
 
 
 
-client.run('ODM3NDA0ODU1MTAyMjEwMDg4.YIsECw.pFrK49dJnREmODcTiXQJFjrUZkk') 
+
